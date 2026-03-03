981b5ceb3643c4717758413279b5f75cfbc7024d
timuzkas <timotiejus.lt@gmail.com>
2026-03-03 23:02:33 +0200

    Fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,19 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
-	  let { children, data } = $props();
-  let { notifications } = data;
+	let { children, data } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{#if notifications.length > 0}
-  <div class="notification-banner bg-accent text-background p-2 text-center">
-    {#each notifications as notification}
-      <p>{notification.message}</p>
-    {/each}
-  </div>
+{#if data.notifications?.length > 0}
+	<div class="notification-banner bg-accent text-background p-2 text-center">
+		{#each data.notifications as notification}
+			<p>{notification.message}</p>
+		{/each}
+	</div>
 {/if}
 
 {@render children()}
